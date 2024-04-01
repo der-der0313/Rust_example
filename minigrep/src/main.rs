68ce886e62bc4e88f5a0de5bf47c613a6f5d0c7c
JiaYipu <2914289170@qq.com>
2024-04-02 00:28:05 +0800

    代码重构，方法写入lib文件

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
+
 
 
 fn main() {
@@ -10,28 +11,12 @@ fn main() {
         println!("解析参数出现问题:{}" ,err);
         process::exit(1);
     });
-    
-    let contents = fs::read_to_string(config.filename)
-                                .expect("读取文件失败");
-    println!("{}",contents);
 
-}
-
-struct Config{
-    query:String,
-    filename:String,
-
-}
-impl Config {
-    fn new(args:&[String]) -> Result<Config,&str>{
-        if args.len() < 3{
-            return Err("参数不足");
-        }
-        Ok(Config{
-            query:args[1].clone(),
-            filename:args[2].clone(),
-        })
+    if let Err(e) = minigrep::run(config){
+        println!("错误:{}",e);
+        process::exit(1)
     }
+
 }
 
 
