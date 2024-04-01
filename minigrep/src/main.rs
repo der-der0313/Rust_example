e10b3bfef6e65d6a74cd47144139bfa9b9c8d604
JiaYipu <2914289170@qq.com>
2024-04-02 00:14:50 +0800

    重构1：抽象函数+添加错误处理

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs;
+use std::process;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("解析参数出现问题:{}" ,err);
+        process::exit(1);
+    });
+    
+    let contents = fs::read_to_string(config.filename)
+                                .expect("读取文件失败");
+    println!("{}",contents);
+
 }
+
+struct Config{
+    query:String,
+    filename:String,
+
+}
+impl Config {
+    fn new(args:&[String]) -> Result<Config,&str>{
+        if args.len() < 3{
+            return Err("参数不足");
+        }
+        Ok(Config{
+            query:args[1].clone(),
+            filename:args[2].clone(),
+        })
+    }
+}
+
+
