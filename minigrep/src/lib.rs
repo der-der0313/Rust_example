a3364da824f21dcef8ab409b5446b29280dea486
JiaYipu <2914289170@qq.com>
2024-04-05 18:20:15 +0800

    重构

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
-use std::fs;
+use std::{fs,env};
+
 
 pub struct Config{
     pub query:String,
     pub filename:String,
+    pub case_sensitive:bool,
 
 }
 impl Config {
@@ -11,15 +13,78 @@ impl Config {
         if args.len() < 3{
             return Err("参数不足");
         }
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_ok();
         Ok(Config{
             query:args[1].clone(),
             filename:args[2].clone(),
+            case_sensitive:case_sensitive,
         })
     }
 }
 
 pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
-    println!("{}",contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results{
+        println!("{}",line);
+    }
+    //println!("{}",contents);
     Ok(())
 }
+
+pub fn search<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if  line.contains(query){
+            result.push(line);
+        }
+    }
+    result
+}
+
+pub fn search_case_insensitive<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "not";
+        let contents = "\
+        not equ
+        bac
+        Rust
+        abc";
+        assert_eq!(vec!["not equ"],search(query,contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "Not";
+        let contents = "\
+        not equ
+        bac
+        Rust
+        abc";
+        assert_eq!(vec!["not equ"],search_case_insensitive(query,contents));
+    }
+}
+
+
