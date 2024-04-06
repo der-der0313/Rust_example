9a519df6e23b46df889536f6f71c932df748d3ac
JiaYipu <2914289170@qq.com>
2024-04-06 23:46:47 +0800

    标准输出和错误输出

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args:Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("解析参数出现问题:{}" ,err);
+        eprintln!("解析参数出现问题:{}" ,err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config){
-        println!("错误:{}",e);
+        eprintln!("错误:{}",e);
         process::exit(1)
     }
 
